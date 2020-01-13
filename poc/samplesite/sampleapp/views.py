26ad72d779b6b89f4e6ae20d53358d908e8017a7
Alejandro Villegas <alejandro.villegas.fernandez@gmail.com>
2020-01-13 12:00:16 +0100

    Added form

--- poc/samplesite/sampleapp/views.py
+++ poc/samplesite/sampleapp/views.py
@@ -1,35 +1,39 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.shortcuts import render
 from django.template import loader
 
 from sampleapp.models import Customer, Administrator, Account
 
+from .forms import NameForm
+
 
 def index(request):
-    if Administrator.objects.all():
-        Customer.objects.all().delete()
-        Administrator.objects.all().delete()
 
-    admin1 = Administrator(name="Admin 1")
-    admin1.save()
-
-    admin2 = Administrator(name="Admin 2")
-    admin2.save()
-
-    customer1 = Customer(name="Juan", last_name="Perez", administrator_id=admin1.id)
-    customer2 = Customer(name="Lucas", last_name="García", administrator_id=admin1.id)
-    customer3 = Customer(name="Marta", last_name="Perez", administrator_id=admin2.id)
-
-    customer1.save()
-    customer2.save()
-    customer3.save()
-
-    acc1 = Account(owner=customer1, iban="DE89370400440532013000")
-    acc2 = Account(owner=customer1, iban="DE89370400440532013150")
-    acc3 = Account(owner=customer3, iban="DE89370400990532013000")
-
-    acc1.save()
-    acc2.save()
-    acc3.save()
+    # if Administrator.objects.all():
+    #     Customer.objects.all().delete()
+    #     Administrator.objects.all().delete()
+    #
+    # admin1 = Administrator(name="Admin 1")
+    # admin1.save()
+    #
+    # admin2 = Administrator(name="Admin 2")
+    # admin2.save()
+    #
+    # customer1 = Customer(name="Juan", last_name="Perez", administrator_id=admin1.id)
+    # customer2 = Customer(name="Lucas", last_name="García", administrator_id=admin1.id)
+    # customer3 = Customer(name="Marta", last_name="Perez", administrator_id=admin2.id)
+    #
+    # customer1.save()
+    # customer2.save()
+    # customer3.save()
+    #
+    # acc1 = Account(owner=customer1, iban="DE89370400440532013000")
+    # acc2 = Account(owner=customer1, iban="DE89370400440532013150")
+    # acc3 = Account(owner=customer3, iban="DE89370400990532013000")
+    #
+    # acc1.save()
+    # acc2.save()
+    # acc3.save()
 
     # admins_str = " ".join(list(map(lambda elem: elem.name, Administrator.objects.all())))
     # customers_str = " ".join(list(
@@ -40,12 +44,31 @@ def index(request):
     #                     "CUSTOMERS: " + customers_str + "\n" +
     #                     "ACCOUNTS: " + accounts_str + "\n")
 
-    administrator_list = [admin1, admin2]
-    customer_list = [customer1, customer2, customer3]
+    administrator_list = Administrator.objects.all()
+    customer_list = Customer.objects.all()
 
     template = loader.get_template('sampleapp/index.html')
+
+    form = NameForm()
+
     context = {
         'administrators': administrator_list,
         'customers': customer_list,
+        'form': form,
     }
     return HttpResponse(template.render(context, request))
+
+
+def added(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            new_admin = Administrator(name=form.cleaned_data['your_name'])
+            new_admin.save()
+
+        template = loader.get_template('sampleapp/added.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+    else:
+        pass
